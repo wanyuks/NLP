d08b2a32bc884aaa16ca6661323d5e86d96b669a
wanyuks <694846404@qq.com>
2019-12-25 18:54:25 +0800

    update textcnn model

--- TextCNN/model.py
+++ TextCNN/model.py
@@ -43,35 +43,3 @@ class TextCNN(nn.Module):
 
         out = self.fc(out)
         return out
-
-# class TextCNN(nn.Module):
-#     def __init__(self, vocab_size, embed_size, num_class, knum,
-#                  kernel_size=[2, 3, 4],dropout_rate=0.2):
-#         super(TextCNN, self).__init__()
-#         self.vocab_size = vocab_size
-#         self.embed_size = embed_size
-#         self.num_class = num_class
-#         Ci = 1
-#         self.knum = knum  ## 每种卷积核的数量
-#         self.kernel_size = kernel_size
-#         self.dropout_rate = dropout_rate
-#
-#         self.embed = nn.Embedding(self.vocab_size, self.embed_size)
-#
-#         self.convs = nn.ModuleList([nn.Conv2d(Ci,self.knum,(K, self.embed_size)) for K in self.kernel_size])  ## 卷积层
-#         self.dropout = nn.Dropout(self.dropout_rate)
-#         self.fc = nn.Linear(len(self.kernel_size) * self.knum, self.num_class)
-#
-#     def forward(self, x):
-#         x = self.embed(x)  # (N,W,D)
-#
-#         x = x.unsqueeze(1)  # (N,Ci,W,D)
-#         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs]  # len(Ks)*(N,Knum,W)
-#         x = [F.max_pool1d(line, line.size(2)).squeeze(2) for line in x]  # len(Ks)*(N,Knum)
-#
-#         x = torch.cat(x, 1)  # (N,Knum*len(Ks))
-#
-#         x = self.dropout(x)
-#         logit = self.fc(x)
-#         return logit
-
